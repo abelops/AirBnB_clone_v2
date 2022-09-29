1cd14eeee47ab2a36fb7c5b1cc003dee8ad3c6b0
Abel Getahun <88226208+abelops@users.noreply.github.com>
2022-09-29 20:31:57 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,6 +5,9 @@ HBNB="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
